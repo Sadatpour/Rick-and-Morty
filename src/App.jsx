0e2127bc4c3115126284b03a650fa2e58b1cc69e
sadatpour <s.m.sadatpour@gmail.com>
2024-06-04 18:12:40 +0330

    feat: fetch data from rick and morty website

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import CharacterDetails from "./components/CharacterDetails";
 import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then(res => res.json())
+      .then(data => setCharacters(data.results));
+  }, []);
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar numOfResult={characters.length} />
       <div className="main">
-        <CharacterList />
+        <CharacterList characters={characters} />
         <CharacterDetails />
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,6 @@
-import { LifebuoyIcon, MusicalNoteIcon } from "@heroicons/react/20/solid";
-import { FilmIcon, MoonIcon, TvIcon } from "@heroicons/react/24/outline";
 import { HeartIcon } from "@heroicons/react/24/outline";
-import { CogIcon } from "@heroicons/react/24/solid";
-import { characters } from "../../data/data";
-export default function Navbar() {
+
+export default function Navbar({ numOfResult }) {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
@@ -11,7 +8,7 @@ export default function Navbar() {
       </div>
       <input type="text" className="text-field" placeholder="search..." />
       <div className="navbar__result">
-        Found <span>{characters.length}</span> characters
+        Found <span>{numOfResult.length}</span> Characters
       </div>
       <button className="heart">
         <HeartIcon className="icon" />
